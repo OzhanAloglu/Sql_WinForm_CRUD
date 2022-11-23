4a85d0f85a3ed085fff7823c48ded8de1b4b7553
Ozhan <ozhanaloglu11@gmail.com>
2022-11-24 02:14:10 +0300

    Add project files.

---
+++ WinFormUygulama/Form1.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace WinFormUygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.mskd_tc = new System.Windows.Forms.MaskedTextBox();
+            this.txt_adısoyadı = new System.Windows.Forms.TextBox();
+            this.txt_yas = new System.Windows.Forms.TextBox();
+            this.radio_bay = new System.Windows.Forms.RadioButton();
+            this.radio_bayan = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_dogumyeri = new System.Windows.Forms.TextBox();
+            this.mskd_tel = new System.Windows.Forms.MaskedTextBox();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_Mezuniyet = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(4, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC KİMLİK NO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(13, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ADI SOYADI:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(30, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "YAŞI:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(13, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "MEZUNİYETİ:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(283, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "DOĞUM YERİ:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(283, 225);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 18);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "TELEFON NO:";
+            // 
+            // mskd_tc
+            // 
+            this.mskd_tc.Location = new System.Drawing.Point(128, 39);
+            this.mskd_tc.Mask = "00000000000";
+            this.mskd_tc.Name = "mskd_tc";
+            this.mskd_tc.Size = new System.Drawing.Size(100, 22);
+            this.mskd_tc.TabIndex = 7;
+            // 
+            // txt_adısoyadı
+            // 
+            this.txt_adısoyadı.Location = new System.Drawing.Point(128, 96);
+            this.txt_adısoyadı.Name = "txt_adısoyadı";
+            this.txt_adısoyadı.Size = new System.Drawing.Size(100, 22);
+            this.txt_adısoyadı.TabIndex = 8;
+            // 
+            // txt_yas
+            // 
+            this.txt_yas.Location = new System.Drawing.Point(128, 154);
+            this.txt_yas.Name = "txt_yas";
+            this.txt_yas.Size = new System.Drawing.Size(100, 22);
+            this.txt_yas.TabIndex = 9;
+            // 
+            // radio_bay
+            // 
+            this.radio_bay.AutoSize = true;
+            this.radio_bay.Location = new System.Drawing.Point(28, 47);
+            this.radio_bay.Name = "radio_bay";
+            this.radio_bay.Size = new System.Drawing.Size(57, 22);
+            this.radio_bay.TabIndex = 11;
+            this.radio_bay.TabStop = true;
+            this.radio_bay.Text = "Bay";
+            this.radio_bay.UseVisualStyleBackColor = true;
+            // 
+            // radio_bayan
+            // 
+            this.radio_bayan.AutoSize = true;
+            this.radio_bayan.Location = new System.Drawing.Point(174, 47);
+            this.radio_bayan.Name = "radio_bayan";
+            this.radio_bayan.Size = new System.Drawing.Size(75, 22);
+            this.radio_bayan.TabIndex = 12;
+            this.radio_bayan.TabStop = true;
+            this.radio_bayan.Text = "Bayan";
+            this.radio_bayan.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radio_bayan);
+            this.groupBox1.Controls.Add(this.radio_bay);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(286, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(286, 85);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CİNSİYET";
+            // 
+            // txt_dogumyeri
+            // 
+            this.txt_dogumyeri.Location = new System.Drawing.Point(408, 156);
+            this.txt_dogumyeri.Name = "txt_dogumyeri";
+            this.txt_dogumyeri.Size = new System.Drawing.Size(100, 22);
+            this.txt_dogumyeri.TabIndex = 14;
+            // 
+            // mskd_tel
+            // 
+            this.mskd_tel.Location = new System.Drawing.Point(408, 225);
+            this.mskd_tel.Mask = "(999) 000-0000";
+            this.mskd_tel.Name = "mskd_tel";
+            this.mskd_tel.Size = new System.Drawing.Size(100, 22);
+            this.mskd_tel.TabIndex = 15;
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.BackColor = System.Drawing.Color.Red;
+            this.btn_ekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_ekle.Location = new System.Drawing.Point(16, 328);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(140, 52);
+            this.btn_ekle.TabIndex = 16;
+            this.btn_ekle.Text = "EKLE";
+            this.btn_ekle.UseVisualStyleBackColor = false;
+            this.btn_ekle.Click += new System.EventHandler(this.btn_ekle_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(162, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 52);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "SİL(tc e göre)";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 397);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(765, 196);
+            this.dataGridView1.TabIndex = 19;
+            // 
+            // txt_Mezuniyet
+            // 
+            this.txt_Mezuniyet.Location = new System.Drawing.Point(130, 210);
+            this.txt_Mezuniyet.Name = "txt_Mezuniyet";
+            this.txt_Mezuniyet.Size = new System.Drawing.Size(100, 22);
+            this.txt_Mezuniyet.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(16, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 54);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "VERİLERİ GÖSTER";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(445, 328);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 52);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "TÜMÜNÜ SİL";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DeepPink;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(601, 328);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(185, 52);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "YENİ KAYIT";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(787, 397);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 20);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Kayıt Sayısı:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(907, 397);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(19, 20);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "0";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Fuchsia;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(299, 328);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 52);
+            this.button5.TabIndex = 26;
+            this.button5.Text = "SİL(seçime göre)";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(601, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(446, 301);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(1053, 599);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_Mezuniyet);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.mskd_tel);
+            this.Controls.Add(this.txt_dogumyeri);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txt_yas);
+            this.Controls.Add(this.txt_adısoyadı);
+            this.Controls.Add(this.mskd_tc);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox mskd_tc;
+        private System.Windows.Forms.TextBox txt_adısoyadı;
+        private System.Windows.Forms.TextBox txt_yas;
+        private System.Windows.Forms.RadioButton radio_bay;
+        private System.Windows.Forms.RadioButton radio_bayan;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_dogumyeri;
+        private System.Windows.Forms.MaskedTextBox mskd_tel;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_Mezuniyet;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WinFormUygulama/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WinFormUygulama
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti = new SqlConnection("Data Source=ALOGLU\\SQLEXPRESS;Initial Catalog=Kayıt;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        int sayac = 1;
+
+        private void verigöster()
+        {
+            baglanti.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from Kayıtbilgi", baglanti);
+            DataSet dt = new DataSet();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt.Tables[0];
+            baglanti.Close();
+
+
+
+        }
+
+
+
+        private void btn_ekle_Click(object sender, EventArgs e)
+        {
+
+
+            SqlCommand komut = new SqlCommand("insert into Kayıtbilgi(TC,ADISOYADI,YAŞI,MEZUNİYETİ,CİNSİYET,DOĞUMYERİ,TELEFON) values (@V1,@V2,@V3,@V4,@V5,@V6,@V7)", baglanti);
+
+            komut.Parameters.AddWithValue("@V1", mskd_tc.Text);
+            komut.Parameters.AddWithValue("@V2", txt_adısoyadı.Text);
+            komut.Parameters.AddWithValue("@V3", txt_yas.Text);
+            komut.Parameters.AddWithValue("@V4", txt_Mezuniyet.Text);
+
+            string cinsi;
+
+            if (radio_bay.Checked)
+            {
+                cinsi = "Bay";
+                komut.Parameters.AddWithValue("@V5", cinsi.ToString());
+            }
+
+            else if (radio_bayan.Checked)
+            {
+                cinsi = "Bayan";
+                komut.Parameters.AddWithValue("@V5", cinsi.ToString());
+            }
+
+            else
+            {
+                MessageBox.Show("Lütfen cinsiyet seçiniz");
+            }
+
+            komut.Parameters.AddWithValue("@V6", txt_dogumyeri.Text);
+            komut.Parameters.AddWithValue("@V7", mskd_tel.Text);
+
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            label8.Text = sayac.ToString();
+            label8.Text = sayac++.ToString();
+            verigöster();
+        }
+
+
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komutsil = new SqlCommand("Delete From Kayıtbilgi where TC=@V1", baglanti);
+            komutsil.Parameters.AddWithValue("@V1", mskd_tc.Text);
+            komutsil.ExecuteNonQuery();
+            label8.Text = sayac--.ToString();
+            if (sayac == -1)
+            {
+                MessageBox.Show("Silinecek bişey kalmadı zorlama!");
+            }
+            baglanti.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            verigöster();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mskd_tc.Clear();
+            txt_adısoyadı.Clear();
+            txt_dogumyeri.Clear();
+            txt_Mezuniyet.Clear();
+            txt_yas.Clear();
+            mskd_tel.Clear();
+
+            SqlCommand komut = new SqlCommand("insert into Kayıtbilgi(TC,ADISOYADI,YAŞI,MEZUNİYETİ,CİNSİYET,DOĞUMYERİ,TELEFON) values (@V1,@V2,@V3,@V4,@V5,@V6,@V7)", baglanti);
+
+            komut.Parameters.AddWithValue("@V1", mskd_tc.Text);
+            komut.Parameters.AddWithValue("@V2", txt_adısoyadı.Text);
+            komut.Parameters.AddWithValue("@V3", txt_yas.Text);
+            komut.Parameters.AddWithValue("@V4", txt_Mezuniyet.Text);
+
+            string cinsi;
+
+            if (radio_bay.Checked)
+            {
+                cinsi = "Bay";
+                komut.Parameters.AddWithValue("@V5", cinsi.ToString());
+            }
+
+            else if (radio_bayan.Checked)
+            {
+                cinsi = "Bayan";
+                komut.Parameters.AddWithValue("@V5", cinsi.ToString());
+            }
+
+            else
+            {
+                MessageBox.Show("Lütfen cinsiyet seçiniz");
+            }
+
+            komut.Parameters.AddWithValue("@V6", txt_dogumyeri.Text);
+            komut.Parameters.AddWithValue("@V7", mskd_tel.Text);
+
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            verigöster();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count>0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+        }
+    }
+}
